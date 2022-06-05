0f9063fbf4b45160ee760b66041b5fdfc7b1e621
AshishSahai <aksahaiashish87@gmail.com>
2022-06-05 14:49:23 +0530

    While Loop added

---
+++ JavaBasics/src/JavaTutorial/WhileLoop.java
@@ -0,0 +1,14 @@
+package JavaTutorial;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		int i=0, count=10;
+		while(i<count) {
+			System.out.println("Prada");
+			i++;
+		}
+
+	}
+
+}
